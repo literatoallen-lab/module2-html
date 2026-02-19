80e88c3841c8ff67bcb51457ebd50426aadcd8d6
literatoallen-lab <literato.allen@gmail.com>
2026-02-19 18:46:13 +0800

    WSAT-M4:JavaScript interactivity added

--- js/script.js
+++ js/script.js
@@ -1,31 +1,63 @@
 console.log("js/script.js");
 
+
+
 const themeToggle = document.getElementById("themeToggle");
+
 themeToggle.addEventListener("click", function() {
     document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        themeToggle.textContent = "Toggle Light Mode";
+    } else {
+        themeToggle.textContent = "Toggle Dark Mode";
+    }
 });
+
+
+
+
 
 const toggleSkills = document.getElementById("toggleSkills");
+
 const skillsSection = document.getElementById("skillsSection");
 
+
+
 toggleSkills.addEventListener("click", function() {
+
     skillsSection.classList.toggle("hidden");
+
 });
+
+
+
+
 
 const submitBtn = document.getElementById("submitBtn");
 
+
+
 submitBtn.addEventListener("click", function (event) {
-    
+
     event.preventDefault();
 
-   
+
+
     const name = document.getElementById("name").value;
+
     const email = document.getElementById("email").value;
 
-  
+
+
     if (name === "" || email === "") {
+
         alert("Please fill in all required fields.");
+
     } else {
+
         alert("Form submitted successfully!");
+
     }
-});
+
+}); 
