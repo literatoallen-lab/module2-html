df9aeeecbd30c8b61ecebc22ceb648f208796fa9
literatoallen-lab <literato.allen@gmail.com>
2026-02-27 00:02:08 +0800

    Add files via upload

---
+++ src/components/Education.jsx
@@ -0,0 +1,42 @@
+function Education() {
+  return (
+    <section className="card">
+      <h2>Education</h2>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Program</th>
+            <th>Institution</th>
+            <th>Year Graduated</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>COLLEGE</td>
+            <td>USTP CDO Campus</td>
+            <td>2028</td>
+          </tr>
+          <tr>
+            <td>SENIOR HIGH SCHOOL</td>
+            <td>ABCCA CDO Campus</td>
+            <td>2024</td>
+          </tr>
+          <tr>
+            <td>HIGH SCHOOL</td>
+            <td>Lapasan National High School</td>
+            <td>2022</td>
+          </tr>
+          <tr>
+            <td>ELEMENTARY</td>
+            <td>East City Central School</td>
+            <td>2018</td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
+  );
+}
+
+export default Education;
